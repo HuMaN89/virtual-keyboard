b45cec299db0b7f525b6f417f3959797f944ccc7
Kudabaev Bekzhan <Human_B_9@mail.ru>
2023-04-13 01:56:42 +0600

    feat: pets is done

--- Shelter/assets/js/modules/cards.js
+++ Shelter/assets/js/modules/cards.js
@@ -6,32 +6,10 @@ function cards() {
     nextArrow = document.querySelector(".arrow-right"),
     prewArrow = document.querySelector(".arrow-left");
   let offset = 0,
-    slideCount = 0,
-    oldCashe = new Set(),
+    petsCount = 0,
     carouselItems,
+    oldCashe = new Set(),
     newCashe = new Set();
-
-  function getRandomArrey(min = 0, max = (slideCount = 8)) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    newCashe.forEach(function (value) {
-      oldCashe.add(value);
-    });
-    newCashe.clear();
-    while (newCashe.size !== 3) {
-      let a = Math.floor(Math.random() * (max - min)) + min;
-      if (!oldCashe.has(a)) {
-        newCashe.add(a);
-      }
-    }
-    oldCashe.clear();
-    newCashe.forEach(function (value) {});
-    let result = [];
-    newCashe.forEach(function (value) {
-      result.push(value);
-    });
-    return result;
-  }
 
   class CarouselItem {
     constructor(
@@ -61,20 +39,42 @@ function cards() {
       const element = document.createElement("div");
       element.classList.add("carousel-item");
       element.innerHTML = `
-        <img
-          src=${this.img}
-          alt=${this.descr}
-          class="carousel-img"
-        />
-        <div class="carousel-title">${this.name}</div>
-        <button class="btn carousel-btn">Learn more</button>`;
+          <img
+            src=${this.img}
+            alt=${this.descr}
+            class="carousel-img"
+          />
+          <div class="carousel-title">${this.name}</div>
+          <button class="btn carousel-btn">Learn more</button>`;
       element.dataset.id = this.id;
       carouselBox.append(element);
       carouselItems = document.querySelectorAll(".carousel-item");
     }
   }
 
-  function init() {
+  function getRandomArrey(min = 0, max = 8) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    newCashe.forEach(function (value) {
+      oldCashe.add(value);
+    });
+    newCashe.clear();
+    while (newCashe.size !== 3) {
+      let a = Math.floor(Math.random() * (max - min)) + min;
+      if (!oldCashe.has(a)) {
+        newCashe.add(a);
+      }
+    }
+    oldCashe.clear();
+    newCashe.forEach(function (value) {});
+    let result = [];
+    newCashe.forEach(function (value) {
+      result.push(value);
+    });
+    return result;
+  }
+
+  function initCards() {
     getRandomArrey().forEach((element) => {
       new CarouselItem(
         data[element].id,
@@ -87,7 +87,7 @@ function cards() {
         data[element].inoculations,
         data[element].diseases,
         data[element].parasites
-      ).render(element);
+      ).render(carouselBox);
     });
 
     for (let el of carouselItems) {
@@ -102,16 +102,16 @@ function cards() {
 
   function next() {
     carouselBox.innerHTML = "";
-    init();
+    initCards();
   }
   function prew() {
     carouselBox.innerHTML = "";
-    init();
+    initCards();
   }
   nextArrow.addEventListener("click", next);
   prewArrow.addEventListener("click", prew);
 
-  init();
+  initCards();
 }
 
 export default cards;
